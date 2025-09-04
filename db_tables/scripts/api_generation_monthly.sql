4e6b2cf7bae550d63b5129e01f80b52795a12524
Nfuu <nicolas@fulghumgap.de>
2025-09-04 10:36:52 +0100

    changed data explorer cutoff

--- db_tables/scripts/api_generation_monthly.sql
+++ db_tables/scripts/api_generation_monthly.sql
@@ -242,11 +242,9 @@ FROM
     LEFT JOIN eu_demand_rank ON generation.country_or_region = eu_demand_rank.country_name
     LEFT JOIN latest_actual_month ON generation.country_or_region = latest_actual_month.country_or_region
 WHERE
-    (
-        (generation.country_or_region = 'World' AND generation_date BETWEEN '2000-01-01' AND '2025-04-01') 
-        OR 
-        (generation_date BETWEEN '2000-01-01' AND latest_actual_month.max_monthly_actual_generation_date)
-    )
+    generation_date >= '2000-01-01'
+    AND (generation_date BETWEEN '2000-01-01' AND latest_actual_month.max_monthly_actual_generation_date)
+    AND NOT (generation.country_or_region = 'World' AND generation_date >= '2025-06-01') 
     AND generation.country_or_region IS NOT NULL
 ORDER BY
     country_or_region,
